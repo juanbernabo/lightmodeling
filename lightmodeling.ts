8dc6736ef5d3031822e2e261fea0a82f9e234c83
Juan Esteban Bernabo <juan.bernabo@gmail.com>
2012-10-10 02:30:52 -0300

    First step into light modeling

---
+++ lightmodeling.ts
@@ -0,0 +1,14 @@
+if (Meteor.isClient) {
+  Meteor.startup(function() {
+    var editor = CodeMirror(document.getElementById("editors"), {
+      value: "function myScript(){return 100;}\n",
+      mode:  "typescript",
+    });
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
