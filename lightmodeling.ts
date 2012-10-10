c983ee31465959bca208e804e18b0576b2e8c764
Juan Esteban Bernabo <juan.bernabo@gmail.com>
2012-10-10 05:07:45 -0300

    Added OMetaJS for in browser evaluation

--- lightmodeling.ts
+++ lightmodeling.ts
@@ -1,10 +1,26 @@
 if (Meteor.isClient) {
+
   Meteor.startup(function() {
     var editor = CodeMirror(document.getElementById("editors"), {
-      value: "\nfunction myScript() { return 100; }\n",
-      mode:  "typescript",
+      value: "\nometa M { ones = 1* }\nalert(M.matchAll([1, 1, 1, 1, 1], \"ones\"))\n\n",
+      mode:  "ometa",
+      parserfile: ["/client/codemirror/ometa/tokenizeometa.js", 
+        "/client/codemirror/ometa/parseometa.js"],
+      stylesheet: "/client/codemirror/ometa/ometacolors.css",
+      path: "/",
+      onKeyEvent: function(editor, evt) {
+        try {
+        if(evt.type=="keyup" && evt.keyCode==13) {
+          eval(translateCode(editor.getValue()));
+        }
+        } catch(e) {
+          alert(e);
+        }
+      }
     });
   });
+  
+
 }
 
 if (Meteor.isServer) {
